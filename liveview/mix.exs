7a9d69fe782b69bee677c2cfd9a49faf5bbff642
MakotoYoshimura(ZenBook) <ymlaedr@gmail.com>
2019-06-07 00:16:24 +0900

    fix: add missing deps(poison)

--- liveview/mix.exs
+++ liveview/mix.exs
@@ -36,13 +36,15 @@ defmodule RotRaven.MixProject do
       {:phoenix, "~> 1.4.6"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
+      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"},
       {:ecto_sql, "~> 3.0"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:poison, "~> 3.1"}
     ]
   end
 
