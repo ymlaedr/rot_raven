b0ac5fac0ad330ba90a226c3ccf5765298cf5d3a
MakotoYoshimura(ZenBook) <ymlaedr@gmail.com>
2019-10-17 05:18:47 +0900

    feat: add seeds

--- umbrella/apps/rot_raven/priv/repo/seeds.exs
+++ umbrella/apps/rot_raven/priv/repo/seeds.exs
@@ -9,3 +9,20 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+
+[
+  %RotRaven.Vehicle.Maker{
+    name: "Honda"
+  },
+  %RotRaven.Vehicle.Maker{
+    name: "Suzuki"
+  },
+  %RotRaven.Vehicle.Maker{
+    name: "Kawasaki"
+  },
+  %RotRaven.Vehicle.Maker{
+    name: "YAMAHA"
+  }
+]
+|> RotRaven.Repo.insert()
